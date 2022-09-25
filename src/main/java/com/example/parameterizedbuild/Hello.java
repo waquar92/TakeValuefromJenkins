ec5f4ca1ea121d862f270ae2eb09e279041ed1ca
waquar92 <waquaralam@socialsurvey.com>
2022-09-25 10:59:52 +0530

    first commit

---
+++ src/main/java/com/example/parameterizedbuild/Hello.java
@@ -0,0 +1,9 @@
+package com.example.parameterizedbuild;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println(System.getenv("FirstName" + "" + "LastName") );
+    }
+}
+
